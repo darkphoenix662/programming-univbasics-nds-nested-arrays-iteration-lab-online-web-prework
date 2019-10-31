5ce4a61bbe340de34e6a0c3ebb04f4bed4054dce
Sean Good <tree662@protonmail.com>
2019-10-31 23:32:51 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,36 +1,41 @@
+require "pry"
 def join_ingredients(src)
   row_index = 0 
   new_array = []
-  while row_index < array_1.count do 
-    element_index = 0 
-    while element_index < array_1.count do
-      new_array << "I love #{array_1[element_index]} and #{array_2[element_index]}"
-      element_index =+ 1
+  while row_index < src.count do 
+    sub_array = src[row_index] 
+      new_array << "I love #{sub_array[0]} and #{sub_array[1]} on my pizza"
+      row_index += 1
     end
-    row_index =+ 1 
+    new_array
   end
-  
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
-end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  counter = 0 
+  new_array = []
+  while counter < src.length do
+    new_array << src[counter].max
+    counter += 1 
+  end
+  new_array
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
-end
+  counter = 0 
+  idx = 0 
+  total = 0 
+  while counter < src.length do
+    if src[counter][0] % 2 == 0 && src[counter][1] % 2 == 0 
+  #     total += src[counter][idx].sum 
+  #   end
+  #   total
+  # end
+    
+
+#   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+#   # if both numbers in the pair are even, then add both those numbers to the
+#   # total
+#   #
+#   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
+#   # the number was even. Review the operator documentation if you've forgotten
+#   # this!
